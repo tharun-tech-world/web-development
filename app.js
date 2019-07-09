283b9cd0383ca73c4da1d490ec56b4809075587c
tharunrgmcet <tharunrgmcet@gmail.com>
2019-07-09 16:41:25 +0530

    Blood Donors Page Added to Github

---
+++ app.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const request=require('request');
+const app=express();
+const path=require('path');
+
+// Load View Engine
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+app.get('/',function(req,res)
+{
+    res.sendFile('BloodWebsite1.html', {root:path.join(__dirname,'./public/html')});
+});
+
+
+app.get('/AboutBloodGroup.html',function(req,res)
+{
+    res.sendFile('AboutBloodGroup.html', {root:path.join(__dirname,'./public/html')});
+});
+
+app.get('/BloodDonators.html',function(req,res)
+{
+    res.sendFile('BloodDonators.html', {root:path.join(__dirname,'./public/html')});
+});
+
+
+app.get('/Createaccount.html',function(req,res)
+{
+    res.sendFile('Createaccount.html', {root:path.join(__dirname,'./public/html')});
+});
+
+app.get('/SignIn.html',function(req,res)
+{
+    res.sendFile('SignIn.html', {root:path.join(__dirname,'./public/html')});
+});
+
+
+
+
+
+
+
+
+var server=app.listen(3000,function() {});
